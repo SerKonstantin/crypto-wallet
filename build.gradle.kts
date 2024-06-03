da458edaea8578a46b24d3dce9129b597b07b03b
serebrianskii <serebrianskiik@gmail.com>
2024-06-03 16:55:05 +0200

    project initialization: first part

---
+++ build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+	application
+	id("org.springframework.boot") version "3.3.0"
+	id("io.spring.dependency-management") version "1.1.5"
+	id("checkstyle")
+	id("jacoco")
+	id("io.freefair.lombok") version "8.6"
+	id("com.github.johnrengelman.shadow") version "8.1.1" // TODO configure fat jar generation for deploy
+	id("com.github.ben-manes.versions") version "0.50.0"
+}
+
+group = "com.konstantin"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.web3j:core:5.0.0")
+	implementation("org.mapstruct:mapstruct:1.5.5.Final")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+	runtimeOnly("org.postgresql:postgresql:42.7.3")
+	runtimeOnly("com.h2database:h2:2.2.224")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+application {
+	mainClass.set("com.konstantin.crypto_wallet.CryptoWalletApplication")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+checkstyle {
+	toolVersion = "10.17.0"
+	configFile = file("config/checkstyle/checkstyle.xml")
+}
+
+jacoco {
+	toolVersion = "0.8.12"
+}
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test)
+	reports {
+		xml.required.set(true)
+		html.outputLocation.set(file("build/reports/jacoco"))
+	}
+}
---
+++ src/test/java/com/konstantin/crypto_wallet/CryptoWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konstantin.crypto_wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
