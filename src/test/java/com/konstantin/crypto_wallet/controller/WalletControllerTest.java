1aa73b8bb8a2744e5a3664992a347145e0db4e0c
serebrianskii <serebrianskiik@gmail.com>
2024-07-01 21:18:18 +0200

    test: add tests for wallet controller

---
+++ src/test/java/com/konstantin/crypto_wallet/controller/WalletControllerTest.java
@@ -0,0 +1,181 @@
+package com.konstantin.crypto_wallet.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.konstantin.crypto_wallet.dto.wallet.WalletCreateDTO;
+import com.konstantin.crypto_wallet.dto.wallet.WalletUpdateDTO;
+import com.konstantin.crypto_wallet.exception.ResourceNotFoundException;
+import com.konstantin.crypto_wallet.model.User;
+import com.konstantin.crypto_wallet.model.Wallet;
+import com.konstantin.crypto_wallet.repository.UserRepository;
+import com.konstantin.crypto_wallet.repository.WalletRepository;
+import com.konstantin.crypto_wallet.util.TestUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.stream.Stream;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class WalletControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private WalletRepository walletRepository;
+
+    @Autowired
+    private ObjectMapper om;
+
+    @Autowired
+    private TestUtils testUtils;
+
+    private SecurityMockMvcRequestPostProcessors.JwtRequestPostProcessor token;
+    private User testUser;
+    private Wallet testWallet;
+
+    @BeforeEach
+    public void setUp() {
+        testUtils.generateData();
+        testUser = testUtils.getTestUser();
+        testWallet = testUtils.getTestWallet();
+        userRepository.save(testUser);
+        walletRepository.save(testWallet);
+        token = jwt().jwt(builder -> builder.subject(testUser.getEmail()));
+    }
+
+    @AfterEach
+    public void clean() {
+        walletRepository.deleteById(testWallet.getId());
+        userRepository.deleteById(testUser.getId());
+    }
+
+    @Test
+    public void testCreateWallet() throws Exception {
+        testUtils.generateData();
+        var walletCreateDTO = new WalletCreateDTO();
+        walletCreateDTO.setAddress(testUtils.getTestWallet().getAddress());
+        walletCreateDTO.setName("Henkilökohtainen");
+
+        var request = post("/api/wallets")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(om.writeValueAsString(walletCreateDTO))
+                .with(token);
+        mockMvc.perform(request).andExpect(status().isCreated());
+
+        var wallets = walletRepository.findByUserId(testUser.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("Wallets not found"));
+        assertThat(wallets).hasSize(2);
+        var wallet = wallets.get(1);
+        assertThat(wallet.getAddress()).isEqualTo(walletCreateDTO.getAddress());
+        assertThat(wallet.getName()).isEqualTo("Henkilökohtainen");
+        assertThat(wallet.getSlug()).isEqualTo("henkilokohtainen");
+
+        mockMvc.perform(request).andExpect(status().isConflict()); // Cannot create wallet with same name
+    }
+
+    @ParameterizedTest
+    @MethodSource("supplyInvalidWalletData")
+    public void testCreateWalletFails(String name, String address) throws Exception {
+        var walletCreateDTO = new WalletCreateDTO();
+        walletCreateDTO.setName(name);
+        walletCreateDTO.setAddress(address);
+
+        var request = post("/api/wallets")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(om.writeValueAsString(walletCreateDTO))
+                .with(token);
+        mockMvc.perform(request).andExpect(status().isBadRequest());
+    }
+
+    private Stream<Arguments> supplyInvalidWalletData() {
+        return Stream.of(
+                Arguments.of("", "address"),
+                Arguments.of(null, "address"),
+                Arguments.of("name", ""),
+                Arguments.of("name", null)
+        );
+    }
+
+    @Test
+    public void testShowWallets() throws Exception {
+        var request = get("/api/wallets");
+        mockMvc.perform(request).andExpect(status().isUnauthorized());
+        mockMvc.perform(request.with(token)).andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$[0].name").value(testWallet.getName()))
+                .andExpect(jsonPath("$[0].address").value(testWallet.getAddress()));
+    }
+
+    @Test
+    public void testShowWallet() throws Exception {
+        var request = get("/api/wallets/" + testWallet.getSlug());
+        mockMvc.perform(request).andExpect(status().isUnauthorized());
+        mockMvc.perform(request.with(token)).andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value(testWallet.getName()))
+                .andExpect(jsonPath("$.address").value(testWallet.getAddress()));
+    }
+
+    @Test
+    public void testUpdateWallet() throws Exception {
+        var walletUpdateDTO = new WalletUpdateDTO();
+        walletUpdateDTO.setName("New Name");
+
+        var request = put("/api/wallets/" + testWallet.getSlug())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(om.writeValueAsString(walletUpdateDTO));
+        mockMvc.perform(request).andExpect(status().isUnauthorized());
+        mockMvc.perform(request.with(token)).andExpect(status().isOk());
+
+        var updatedWallet = walletRepository.findById(testWallet.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("Wallet not found"));
+        assertThat(updatedWallet.getName()).isEqualTo("New Name");
+        assertThat(updatedWallet.getSlug()).isEqualTo("newname");
+    }
+
+    @Test
+    public void testUpdateWalletFails() throws Exception {
+        var walletUpdateDTO = new WalletUpdateDTO();
+        walletUpdateDTO.setName("");
+
+        var request = put("/api/wallets/" + testWallet.getSlug())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(om.writeValueAsString(walletUpdateDTO))
+                .with(token);
+        mockMvc.perform(request).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testDeleteWallet() throws Exception {
+        assertThat(walletRepository.findById(testWallet.getId())).isPresent();
+        var request = delete("/api/wallets/" + testWallet.getSlug())
+                .param("confirmation", "I want to delete this wallet permanently");
+        mockMvc.perform(request).andExpect(status().isUnauthorized());
+        mockMvc.perform(request.with(token)).andExpect(status().isNoContent());
+        assertThat(walletRepository.findById(testWallet.getId())).isEmpty();
+    }
+}
--- src/test/java/com/konstantin/crypto_wallet/util/TestUtils.java
+++ src/test/java/com/konstantin/crypto_wallet/util/TestUtils.java
@@ -2,6 +2,7 @@ package com.konstantin.crypto_wallet.util;
 
 import com.konstantin.crypto_wallet.config.Constants;
 import com.konstantin.crypto_wallet.model.User;
+import com.konstantin.crypto_wallet.model.Wallet;
 import lombok.Getter;
 import net.datafaker.Faker;
 import org.instancio.Instancio;
@@ -9,6 +10,8 @@ import org.instancio.Select;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
 
 @Getter
 @Component
@@ -19,28 +22,51 @@ public class TestUtils {
 
     private String passwordInput;
     private User testUser;
+    private Wallet testWallet;
 
     public void generateData() {
+        testUser = generateUser();
+        testWallet = generateWallet(testUser);
+        testUser.getWallets().add(testWallet);
+    }
+
+    private User generateUser() {
         var faker = new Faker();
         passwordInput = faker.internet().password(Constants.MIN_PASSWORD_LENGTH, 100);
         var password = passwordEncoder.encode(passwordInput);
-        testUser = Instancio.of(User.class)
+
+        return Instancio.of(User.class)
                 .ignore(Select.field(User::getId))
-                .supply(Select.field(User::getNickname), () -> generateNickname(Constants.MIN_NICKNAME_LENGTH))
+                .supply(Select.field(User::getNickname), this::generateNickname)
                 .supply(Select.field(User::getEmail), () -> faker.internet().emailAddress())
                 .supply(Select.field(User::getPassword), () -> password)
                 .ignore(Select.field(User::getCreatedAt))
                 .ignore(Select.field(User::getUpdatedAt))
-                .ignore(Select.field(User::getWallets))
+                .supply(Select.field(User::getWallets), () -> new ArrayList())
                 .create();
     }
 
-    public String generateNickname(int minLength) {
+    private Wallet generateWallet(User user) {
+        var faker = new Faker();
+        var walletName = faker.lorem().word() + " wallet";
+        var slug = walletName.replaceAll(" ", "").toLowerCase();
+        return Instancio.of(Wallet.class)
+                .ignore(Select.field(Wallet::getId))
+                .supply(Select.field(Wallet::getAddress), () -> faker.regexify("[a-f0-9]{64}"))
+                .supply(Select.field(Wallet::getName), () -> walletName)
+                .supply(Select.field(Wallet::getSlug), () -> slug)
+                .ignore(Select.field(Wallet::getCreatedAt))
+                .ignore(Select.field(Wallet::getUpdatedAt))
+                .supply(Select.field(Wallet::getUser), () -> user)
+                .create();
+    }
+
+    private String generateNickname() {
         var faker = new Faker();
         String nickname;
         do {
             nickname = faker.name().firstName();
-        } while (nickname.length() <= minLength);
+        } while (nickname.length() <= Constants.MIN_NICKNAME_LENGTH);
         return nickname;
     }
 }
