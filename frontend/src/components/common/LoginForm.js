d459e72d97be2495e46bda583a928acc2455d483
serebrianskii <serebrianskiik@gmail.com>
2024-09-01 16:28:06 +0200

    refactor: separate fetch logic from visualisation

--- frontend/src/components/common/LoginForm.js
+++ frontend/src/components/common/LoginForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import config from '../../../src/config';
+import config from '../../config/config';
 
 const apiClient = axios.create({
   baseURL: config.apiBaseUrl,
