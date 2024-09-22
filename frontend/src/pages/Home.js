721899a271cca3a6c8dcf426e3038018e1af1d18
serebrianskii <serebrianskiik@gmail.com>
2024-09-22 21:51:52 +0200

    feat: add app description to home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,7 +5,17 @@ function Home() {
   return (
     <div>
       <h1>Welcome to Crypto Wallet App</h1>
-      <p>TODO Promo/Description</p>
+      <p>
+        Manage your Ethereum wallets in one place. The Crypto Wallet App allows
+        you to add multiple wallets and perform transactions. Currently, the app
+        only supports Ethereum transactions.
+      </p>
+
+      <p>
+        For demonstration purposes, the app uses the Sepolia testnet, allowing
+        you to explore wallet management features safely without using real
+        assets. Try it out now!
+      </p>
 
       <Link to="/login">
         <button>Login</button>
@@ -13,6 +23,23 @@ function Home() {
       <Link to="/register">
         <button>Register</button>
       </Link>
+
+      <h3>Main Features:</h3>
+      <ul>
+        <li>Register an account with secure authentication using JWT tokens</li>
+        <li>Add and manage multiple Ethereum wallets</li>
+        <li>Wallet balances are fetched directly from the blockchain</li>
+        <li>Send and receive Ethereum transactions</li>
+        <li>View transactions history</li>
+      </ul>
+
+      <h3>Technologies Used:</h3>
+      <ul>
+        <li>React, JavaScript (Frontend)</li>
+        <li>Spring Boot, PostgreSQL (Backend)</li>
+        <li>Web3.js for blockchain interaction</li>
+        <li>Docker, Sentry, GitHub Actions CI</li>
+      </ul>
     </div>
   );
 }
